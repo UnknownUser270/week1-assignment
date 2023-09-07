8133a5059e519eb42cc083708ec8c92316a254f1
santana <santana@localhost>
2023-09-07 16:56:08 -0700

    Commit

---
+++ src/main/java/com/mycompany/week1/javaa/Week1Javaa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.week1.javaa;
+
+/**
+ *
+ * @author santana
+ */
+public class Week1Javaa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
